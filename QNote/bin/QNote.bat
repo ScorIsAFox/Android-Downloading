6330a2d1afa3c3fe6bc8157e527c0c6d51160ae6
ScorIsAFox <101972963+ScorIsAFox@users.noreply.github.com>
2022-07-27 17:39:09 +0800

    Delete QNote directory

--- QNote/bin/QNote.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-D:
-cd D:\1-Project\Java Projects\QNote\src
-javac Enter.java
-start javaw Enter
-taskkill /f /im cmd.exe
--- QNote/src/Enter.java
+++
@@ -1,8 +0,0 @@
-
-public class Enter {
-	
-	public static void main(String[] args) {
-		new NoteFrame().setVisible(true);
-	}
-	
-}
